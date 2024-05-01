7341f9c09f98d163f19d44a42cecc69f4de7f1e2
liwanye <evan.wy.li@gmail.com>
2024-05-01 15:20:29 +0800

    adding code 20240501

--- mobilevla/models/action_head.py
+++ mobilevla/models/action_head.py
@@ -517,6 +517,270 @@ class GPTDecoder(ActionDecoder):
         return 'gpt_{}_'.format(self.hidden_size, )
 
 
+class DiffusionDecoder(ActionDecoder):
+    def __init__(
+            self,
+            feature_dim: int,
+            window_size: int,
+            history_len=None,
+            horizon=32,
+            input_dim: int = 7,  # dim of vectors to be diffused
+            diffusion_step_embed_dim=256,
+            down_dims=[256, 512, 1024],
+            kernel_size=3,
+            n_groups=8,
+            cond_predict_scale=False,
+            n_timesteps=150,
+            clip_denoised=False,
+            predict_epsilon=True,
+            normalizer=LinearNormalizer()
+    ):
+        super(DiffusionDecoder, self).__init__()
+        self.feature_dim = feature_dim
+        self.horizon = horizon
+        self.window_size = window_size
+        if history_len is None:
+            history_len = window_size
+        self.history_len = history_len
+        self.history_memory = []
+        self.normalizer = normalizer
+        self.data_dim = input_dim
+
+        self.model = ConditionalUnet1D(
+            input_dim,
+            global_cond_dim=feature_dim,
+            # global_cond_dim=None,
+            diffusion_step_embed_dim=diffusion_step_embed_dim,
+            down_dims=down_dims,
+            kernel_size=kernel_size,
+            n_groups=n_groups,
+            cond_predict_scale=cond_predict_scale,
+        )
+
+        betas = cosine_beta_schedule(n_timesteps)
+        alphas = 1.0 - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+
+        self.n_timesteps = int(n_timesteps)
+        self.clip_denoised = clip_denoised
+        self.predict_epsilon = predict_epsilon
+
+        self.register_buffer("betas", betas)
+        self.register_buffer("alphas_cumprod", alphas_cumprod)
+        self.register_buffer("alphas_cumprod_prev", alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer("sqrt_alphas_cumprod", torch.sqrt(alphas_cumprod))
+        self.register_buffer(
+            "sqrt_one_minus_alphas_cumprod", torch.sqrt(1.0 - alphas_cumprod)
+        )
+        self.register_buffer(
+            "log_one_minus_alphas_cumprod", torch.log(1.0 - alphas_cumprod)
+        )
+        self.register_buffer(
+            "sqrt_recip_alphas_cumprod", torch.sqrt(1.0 / alphas_cumprod)
+        )
+        self.register_buffer(
+            "sqrt_recipm1_alphas_cumprod", torch.sqrt(1.0 / alphas_cumprod - 1)
+        )
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = (
+                betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
+        )
+        self.register_buffer("posterior_variance", posterior_variance)
+
+        # log calculation clipped because the posterior variance
+        # is 0 at the beginning of the diffusion chain
+        self.register_buffer(
+            "posterior_log_variance_clipped",
+            torch.log(torch.clamp(posterior_variance, min=1e-20)),
+        )
+        self.register_buffer(
+            "posterior_mean_coef1",
+            betas * np.sqrt(alphas_cumprod_prev) / (1.0 - alphas_cumprod),
+        )
+        self.register_buffer(
+            "posterior_mean_coef2",
+            (1.0 - alphas_cumprod_prev) * np.sqrt(alphas) / (1.0 - alphas_cumprod),
+        )
+
+    def get_loss_weights(self, action_weight, discount, weights_dict):
+        """
+        sets loss coefficients for trajectory
+        action_weight   : float
+            coefficient on first action loss
+        discount   : float
+            multiplies t^th timestep of trajectory loss by discount**t
+        weights_dict    : dict
+            { i: c } multiplies dimension i of observation loss by c
+        """
+        self.action_weight = action_weight
+
+        dim_weights = torch.ones(self.action_dim, dtype=torch.float32)
+
+        # set loss coefficients for dimensions of observation
+        if weights_dict is None:
+            weights_dict = {}
+        for ind, w in weights_dict.items():
+            dim_weights[self.action_dim + ind] *= w
+
+        # decay loss with trajectory timestep: discount**t
+        discounts = discount ** torch.arange(self.horizon, dtype=torch.float)
+        discounts = discounts / discounts.mean()
+        loss_weights = torch.einsum("h,t->ht", discounts, dim_weights)
+        loss_weights = loss_weights.unsqueeze(1).clone()
+
+        return loss_weights
+
+    # ------------------------------------------ sampling ------------------------------------------#
+
+    def predict_start_from_noise(self, x_t, t, noise):
+        """
+        if self.predict_epsilon, model output is (scaled) noise;
+        otherwise, model predicts x0 directly
+        """
+        if self.predict_epsilon:
+            return (
+                    extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t
+                    - extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise
+            )
+        else:
+            return noise
+
+    def q_posterior(self, x_start, x_t, t):
+        posterior_mean = (
+                extract(self.posterior_mean_coef1, t, x_t.shape) * x_start
+                + extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+        )
+        posterior_variance = extract(self.posterior_variance, t, x_t.shape)
+        posterior_log_variance_clipped = extract(
+            self.posterior_log_variance_clipped, t, x_t.shape
+        )
+        return posterior_mean, posterior_variance, posterior_log_variance_clipped
+
+    def p_mean_variance(self, x, t, local_cond=None, global_cond=None, returns=None):
+
+        if returns is not None:
+            # epsilon could be epsilon or x0 itself
+            epsilon_cond = self.model(x, t, local_cond, global_cond, returns, use_dropout=False)
+            epsilon_uncond = self.model(x, t, local_cond, global_cond, returns, force_dropout=True)
+            epsilon = epsilon_uncond + self.condition_guidance_w * (
+                    epsilon_cond - epsilon_uncond
+            )
+        else:
+            epsilon = self.model(x, t, local_cond, global_cond)
+
+        t = t.detach().to(torch.int64)
+        x_recon = self.predict_start_from_noise(x, t=t, noise=epsilon)
+
+        if self.clip_denoised:
+            x_recon.clamp_(-1.0, 1.0)
+        else:
+            assert RuntimeError()
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
+            x_start=x_recon, x_t=x, t=t
+        )
+        return model_mean, posterior_variance, posterior_log_variance
+
+    @torch.no_grad()
+    def p_sample(self, x, t, local_cond=None, global_cond=None, returns=None):
+        b = x.shape[0]
+        model_mean, _, model_log_variance = self.p_mean_variance(
+            x=x, t=t, local_cond=local_cond, global_cond=global_cond, returns=returns
+        )
+        noise = 0.5 * torch.randn_like(x)
+        # no noise when t == 0
+        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
+        return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
+
+    @torch.no_grad()
+    def p_sample_loop(
+            self, cond_data, cond_mask, local_cond=None, global_cond=None, returns=None, verbose=False,
+            return_diffusion=False, **kwargs
+    ):
+        device = self.betas.device
+
+        batch_size = cond_data.shape[0]
+        x = torch.randn(
+            size=cond_data.shape,
+            dtype=cond_data.dtype,
+            device=cond_data.device
+        )
+
+        if return_diffusion:
+            diffusion = [x]
+
+        x[cond_mask] = cond_data[cond_mask]
+        progress = Progress(self.n_timesteps) if verbose else Silent()
+        for i in reversed(range(0, self.n_timesteps)):
+
+            timesteps = torch.full((batch_size,), i, device=device, dtype=torch.long)
+            # 1. predict model output and replace sample
+            x = self.p_sample(x, timesteps, local_cond, global_cond, returns)
+
+            # 2. apply conditioning
+            x[cond_mask] = cond_data[cond_mask]
+
+            progress.update({"t": i})
+
+            if return_diffusion:
+                diffusion.append(x)
+
+        progress.close()
+
+        if return_diffusion:
+            return x, torch.stack(diffusion, dim=1)
+        else:
+            return x
+
+    @torch.no_grad()
+    def conditional_sample(self, cond_data, cond_mask, local_cond=None, global_cond=None, returns=None,
+                           action_seq_len=None, *args, **kwargs):
+        """
+        conditions : [ (time, state), ... ]
+        """
+
+        # horizon = action_seq_len or self.action_seq_len
+        # batch_size = len(list(cond_data.values())[0])
+        # shape = (batch_size, horizon, self.action_dim) # cond_data.shape
+        return self.p_sample_loop(cond_data, cond_mask, local_cond, global_cond, returns, *args, **kwargs)
+
+    # ------------------------------------------ training ------------------------------------------#
+
+    def q_sample(self, x_start, t, noise=None):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+        sample = (
+                extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start
+                + extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise
+        )
+
+        return sample
+
+    def forward(
+            self,
+            x,
+            t,
+            local_cond=None,
+            global_cond=None,
+            **kwargs
+    ):
+        return self.model(x, t, local_cond, global_cond)
+
+    def act(
+            self,
+            input_feature: torch.Tensor,
+    ) -> torch.Tensor:
+        pred_actions, self.hidden_state = self(
+            input_feature, self.hidden_state
+        )
+
+        raise NotImplementedError
+
+
 if __name__ == "__main__":
     model = GPTDecoder(128, 24)
     in_feat = torch.randn((4 * 24, 12, 128))
